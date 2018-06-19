a7558f317568ab45718fd3b867704c5c3b21bd08
MajBog <maja.boguslawska@gmail.com>
2018-06-19 22:11:41 +0200

    session set

--- app_ptest.py
+++ app_ptest.py
@@ -1,14 +1,26 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, session
+import os
 
 
 app = Flask(__name__)
+app.secret_key = os.urandom(50)
+
 
 @app.route('/start', methods=['GET', 'POST'])
 def get_user_name():
     if request.method == 'GET':
         return render_template('user_name_form.html')
     else:
-        return 'OOPS'
+        session['user'] = request.form['user_name']
+        return render_template(
+            'welcome_user.html',
+            user=session['user']
+        )
+
+    
+@app.route('/test', methods=['GET', 'POST'])
+def do_the_test():
+    pass
 
 
 if __name__ == '__main__':
