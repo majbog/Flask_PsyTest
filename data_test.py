1c2ed9e031b8aa4cb4c1f0cecf3fcedc1f2d7644
MajBog <maja.boguslawska@gmail.com>
2018-06-25 23:33:37 +0200

    data prepared

---
+++ data_test.py
@@ -0,0 +1,69 @@
+houses ={
+    'Gryffindor': 'sample description1',
+    'Slytherin': 'sample description2',
+    'Ravenclaw': 'sample description3',
+    'Hufflepuff': 'sample description4'
+}
+
+questions = (
+    """You’ve made it to Hogwarts, which means you’ve already bought a wand from Ollivander’s. 
+    What material is at its core?""",
+    """During the end-of-year exams, you notice that one of your classmates was using an enchanted quill. 
+    You come top of the class anyway, but they are second. What do you do?""",
+    """You would be most hurt if a person called you...""",
+    """You're locked in a duel with a skilled opponent. They fire an unknown spell at you, and you shout…""",
+    """It's your fifth year at Hogwarts, and you've just received a Howler from your parents. What for?""",
+    """Which of these Dumbledore quotations speaks to you?""",
+    """Which of these most accurately describes your relationship with your closest friends?""",
+    """Which of your skills are you most proud of?""",
+    """The first Quidditch match of the season is approaching, and you can't wait to get involved. 
+    What role are you playing?""",
+    """You're allowed a pet at Hogwarts: an owl, a cat, or a toad. Which do you bring?""",
+    """It's Saturday, you've finished your homework, and you have some free time.
+     You decide to spend some time away from your common room. Where do you go?""",
+    """What would you see in the Mirror of Erised?""",
+    """Choose a Deathly Hallow.""",
+    """Which path do you intend to follow after leaving Hogwarts?""",
+    """And finally: We know that the Sorting Hat takes into account your preferences. 
+    So which Hogwarts house do you feel you identify with most closely?"""
+)
+
+answers = (
+    ["Phoenix Feather", 1, 1], ["Dragon Heartstring", 1, 2], ["Unicorn Hair", 1, 3],
+    ["Tell the professor immediately - cheating is wrong, no matter what", 2, 4],
+    ["Nothing, but if I hadn't come top of the class, I'd definitely tell the professor", 2, 3],
+    ["Encourage the other student to admit what they'd done to the professor.", 2, 1],
+    ["Give them a high five for managing to sneak the quill into the exam", 2, 2],
+    ["Weak", 3, 1], ["Ignorant", 3, 3], ["Unkind", 3, 4], ["Boring", 3, 2],
+    ["Expelliarmus!", 4, 1], ["Protego!", 4, 4], ["Stupefy!", 4, 3], ["Crucio!", 4, 2],
+    ["Sneaking into the Forbidden Forest at night on a dare.", 5, 1],
+    ["Getting caught cheating in my Divination O.W.L.", 5, 2],
+    ["Being put in detention after I was caught in the library after hours.", 5, 3],
+    ["Nothing! I'd never do anything to warrant a Howler.", 5, 4],
+    ["Pity the living, and above all, those who live without love.", 6, 4],
+    ["Words are, in my not-so-humble opinion, our most inexhaustible source of magic.", 6, 3],
+    ["It matters not what someone is born, but what they grow to be.", 6, 1],
+    ["It does not do to dwell on dreams and forget to live.", 6, 2],
+    ["I love surrounding myself with people – the more friends I have, the better!", 7, 4],
+    ["I have a few very close friends that I would trust with my life.", 7, 1],
+    ["I tend to be wary around new people, so don't make new friends often.", 7, 3],
+    ["I find myself becoming friends with people who can help me to succeed.", 7, 2],
+    ["My ability to absorb new information.", 8, 3], ["My ability to make new friends.", 8, 4],
+    ["My ability to get what I want.", 8, 2], ["My ability to keep secrets.", 8, 1],
+    ["Seeker. I want the glory!", 9, 1], ["Chaser. I like to be involved, and work as part of the team.", 9, 3],
+    ["Beater. I like having all that power.", 9, 2],
+    ["I'll be in the crowd, making sure supporter morale is high!", 9, 4],
+    ["Owl", 10, 3], ["Cat", 10, 1], ["Toad", 10, 4], ["Nothing. I can't be trusted to look after a pet!", 10, 2],
+    ["The Forbidden Forest", 10, 1], ["The library", 10, 3], ["The kitchens", 10, 4], ["The Room of Requirement", 10, 2],
+    ["Myself, surrounded by riches", 11, 2], ["Myself, surrounded by my loving family and friends.", 11, 4],
+    ["Myself, knowledgable above all.", 11, 3], ["Myself, experiencing a marvellous adventure.", 11, 1],
+    ["The Elder Wand", 12, 2], ["The Resurrection Stone", 12, 4], ["The Cloak of Invisibility", 12, 3],
+    ["I'd join the Ministry – I want to make a difference in the world.", 13, 2],
+    ["I think I'd travel for a while before committing to a career.", 13, 1],
+    ["I'd settle down and start a family as soon as possible!", 13, 4],
+    ["I'd continue to work hard in order to achieve as much success as possible.", 13, 3],
+    ["Gryffindor", 14, 1], ["Slytherin", 14, 2], ["Ravenclaw", 14, 3], ["Hufflepuff", 14, 4]
+)
+
+
+
--- models.py
+++ models.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+from data_test import questions, answers, houses
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:coderslab@localhost:5432/pt_db'
 db = SQLAlchemy(app)
@@ -30,34 +32,29 @@ class Question(db.Model):
 class Answer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.String(250), nullable=False)
-    quest = db.Column(db.Integer, db.ForeignKey('question.id'), nullable=False)
+    question_id = db.Column(db.Integer, db.ForeignKey('question.id'), nullable=False)
     house_id = db.Column(db.Integer, db.ForeignKey('house.id'))
 
-    def __init__(self, text, quest, house_id,):
+    def __init__(self, text, question_id, house_id,):
 
         self.text = text
-        self.quest = quest
+        self.question_id = question_id
         # self.house = house
         self.house_id = house_id
 
 
 if __name__ == '__main__':
-    # db.create_all()
-    # print('created tables')
-    # gryffindor = House('Gryffindor', 'sample description1')
-    # hufflepuff = House('Hufflepuff', 'sample description2')
-    # ravenclaw = House('Ravenclaw', 'sample description3')
-    # slytherin = House('Slytherin', 'sample description4')
-    # db.session.add(gryffindor)
-    # db.session.add(hufflepuff)
-    # db.session.add(ravenclaw)
-    # db.session.add(slytherin)
-    # db.session.commit()
-    # print(gryffindor.id)
-    q1 = Question('sample question1')
-    a1 = Answer('sample answer1', 1, 1)
-    db.session.add(q1)
-    db.session.add(a1)
+    db.create_all()
+    print('created tables')
+    # pupulate database
+    for key in houses:
+        h = House(key, houses[key])
+        db.session.add(h)
+    for q in questions:
+        db.session.add(q)
+    for answer in answers:
+        a = Answer(answer[0], answer[1], answer[2])
+        db.session.add(a)
     db.session.commit()
-    print(a1.id)
+
 
