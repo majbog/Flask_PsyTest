ca67632ecd39c2e1db2e008d758d1dc6c9cc9a5c
MajBog <maja.boguslawska@gmail.com>
2018-07-03 22:29:29 +0200

    test sequence js ok

--- static/app.js
+++ static/app.js
@@ -2,8 +2,8 @@ $(function () {
 
     console.log("It's working!");
 
-    $('#intro-test').css('display', 'none');
-    $('#go-further').css('display', 'none');
+    $('.question-content').css('display', 'none');
+
     $(document).ready(function () {
         $('#intro-test').fadeIn(1300);
     });
@@ -14,11 +14,27 @@ $(function () {
 
     $('#go-further').click(function () {
         $(this).fadeOut(1300);
-        $('#intro-test').fadeOut(1300);
+        $('#intro-test').fadeOut(1400);
         setTimeout(function () {
-            $('#prepare').fadeIn(1300)
-        }, 2000)
+            $('#prepare').fadeIn(1400)
+        }, 2100);
+        setTimeout(function () {
+            $('#prepare').fadeOut(1400)
+        },4100);
+        setTimeout(function () {
+            $('#main-test label:first-of-type').fadeIn(1400)
+        },6000)
+    });
+
+
+
+    $('.answer-radio').each(function () {
+        $(this).click(function () {
+            $(this).parent().fadeOut(50);
+            $(this).parent().next().fadeIn(3000);
+        })
     })
+
 
 
 });
