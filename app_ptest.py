ff5a815c83b767d91078ff36cb69355a4ae20acd
MajBog <maja.boguslawska@gmail.com>
2018-07-04 00:07:42 +0200

    handle form input data

--- app_ptest.py
+++ app_ptest.py
@@ -34,7 +34,28 @@ def do_the_test():
         questions = Question.query.all()
         return render_template('test_form.html', questions=questions)
     else:
-        answers = []
+        questions = Question.query.all()
+        quest_ids = [question.id for question in questions]
+        answers_value = []
+        for question in quest_ids:
+            val = request.form['answer-option{}' .format(question)]
+            answers_value.append(val)
+        gryffindor_result = {'id_house': 1, 'score': len([val for val in answers_value if val == 1])}
+        hafflepuff_result = {'id_house': 4, 'score': len([val for val in answers_value if val == 4])}
+        slytherin_result = {'id_house': 2, 'score': len([val for val in answers_value if val == 2])}
+        ravenclaw_result = {'id_house': 3, 'score': len([val for val in answers_value if val == 3])}
+        all_results = [gryffindor_result, hafflepuff_result, slytherin_result, ravenclaw_result]
+        highest_score = [all_results[0]]
+        for result in all_results:
+            if result['score'] > highest_score[0]['score']:
+                highest_score.pop()
+                highest_score.append(result)
+        selected_house = House.query.get(highest_score[0]['id_house'])
+        return 'not finished  but House id is {}' .format(highest_score[0]['id_house'])
+
+
+
+
 
 
 
